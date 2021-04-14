ce9761cbf369315fdd908268533c9d9360032e8c
Colin Morrison <colinjmmorris@gmail.com>
2021-04-14 08:33:54 +0100

    Error found, in defining dinosaur

---
+++ models/park.js
@@ -0,0 +1,79 @@
+const Dinosaur = require('../models/dinosaur.js');
+const Park = function(name, ticket_price, dinosaurs){
+    this.name = name;
+    this.ticket_price = ticket_price;
+    this.dinosaurs = dinosaurs;
+}
+
+Park.prototype.add_dinosaur = function(dinosaur){
+    this.dinosaurs.push(dinosaur);
+}
+
+Park.prototype.remove_dinosaur = function(dinosaur){
+   index = this.dinosaurs[dinosaur];
+   this.dinosaurs.splice(index,1);
+}
+
+Park.prototype.best_attraction = function(){
+    // Set a baseline dinosaur, dinosaurMax, for comparison, with null values, or 0 guests
+    // Loop through each dino, in the dinosaurs list
+    // If the dino guests attracted > baseline dinosaur guests, replace the baseline with that dino, say dino1
+    // Then next item in the array, if dino2 guests attracted > dino 1 guests, repeat as above etc.
+    // End loop
+    // Return our final dinosaurMax
+
+    let dinosaurMax = new Dinosaur(null, null, 0);
+    // let dinosaurMax = [];
+    // let dinosaurMax = {};
+
+    for (let dino of this.dinosaurs){ // for each dino object in the dinosaurs list
+        // let dino_value = dino['guestsAttractedPerDay'];
+        // let dino_max_value = dinosaurMax['guestsAttractedPerDay'];
+        
+        let dino_value = dino.guestsAttractedPerDay; // Grabs the value of the guests key from dino obect
+        let dino_max_value = dinosaurMax.guestsAttractedPerDay;
+        if (dino_value > dino_max_value){
+            dinosaurMax = new Dinosaur(dino.species, dino.diet, dino.guestsAttractedPerDay);
+            
+            // dinosaurMax = [dino.species, dino.diet, dino.guestsAttractedPerDay]
+        };
+    }
+    return dinosaurMax.species;
+
+    // return this.dinosaurs; // Did this for a test, interestingly it is only returning [150, 80], not the whole dino??
+
+
+    // Also tried:
+    // for (i = 0; i < this.dinosaurs.length; i++){
+    //     // if (i.guestsAttractedPerDay > dinosaur1.guestsAttractedPerDay){
+    //     //     this.dinosaur1 = i;
+    //     // }
+    //     if (i.guestsAttractedPerDay > (i-1).guestsAttractedPerDay){
+    //         dinosaur1 = i;
+    //     }
+    // }
+    // return dinosaur1;
+    // return this.dinosaur1
+
+    // Alternative way to try make work:
+    let max_num_guests = Math.max(this.dinosaurs.guestsAttractedPerDay);
+    return max_num_guests.guestsAttractedPerDay;
+    // Then find which dinosaur matches up with the max value found
+    // This avoids having to import dinosaur here
+
+}
+
+Park.prototype.same_species = function(search_species){
+    let list_of_dinos = [];
+    for (dino of this.dinosaurs){
+        if (dino.species === search_species){
+            list_of_dinos.push(dino);
+        }
+    }
+    return list_of_dinos;
+    // This is returning an empty list. So the return is okay
+    // Its like it cant find dino.species, or they simply do not match
+}
+
+
+module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,23 +4,59 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+  let park;
+
   beforeEach(function () {
+    let dinosaur_1 = new Dinosaur ('Big Ones', 'Pineapples', 150)
+    let dinosaur_2 = new Dinosaur ('Small bois', 'Gluten', 80)
+    let dinosaurs = [dinosaur_1, dinosaur_2]
+    park = new Park('Historical Thiccs', 185.00, dinosaurs);
+  });
 
-  })
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Historical Thiccs');
+  });
 
-  it('should have a name');
+  it('should have a ticket price', function(){
+    const actual = park.ticket_price;
+    assert.strictEqual(actual, 185.00);
+  });
 
-  it('should have a ticket price');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 2);
+  });
+  
+  it('should be able to add a dinosaur to its collection',function(){
+    let dinosaur_3 = ('Riggidy Rex', 'Pizza', 1000);
+    park.add_dinosaur(dinosaur_3)
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 3);
+  });
 
-  it('should have a collection of dinosaurs');
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to remove a dinosaur from its collection', function(){
+    let dinosaur = this.dinosaur_2;
+    park.remove_dinosaur(dinosaur);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual,1);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    // Could grab a whole dinosaur, but it is nicer if we just grab a species
+    const actual = park.best_attraction();
+    // const actual = park.dinosaurMax
+    assert.strictEqual(actual, 'Big Ones');
+    // Note, we will return just the species from best_attraction eventually
+    // For now, just trying to return the dinosaur itself to see what happens in the terminal
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
-
-  it('should be able to find all dinosaurs of a particular species');
+  xit('should be able to find all dinosaurs of a particular species', function(){
+    search = 'Big Ones';
+    const actual = park.same_species(search);
+    assert.strictEqual(actual,1);
+  });
 
   it('should be able to calculate the total number of visitors per day');
 
